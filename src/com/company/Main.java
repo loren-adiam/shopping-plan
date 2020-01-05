88a3572109a3c01ae824a47108a04149c7863da9
Nerol Karic <karicnerol@gmail.com>
2020-01-05 15:04:35 +0100

    Added checking if the list is empty for 3 options

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,14 +12,14 @@ public class Main {
     public static void main(String[] args) {
         int userInput;
         System.out.println("____________________________________________________________________________");
-        System.out.println("Fruit Salad shopping plan");
+        System.out.println("FRUIT SALAD SHOPPING PLAN");
 
         do {
             System.out.println("\nOptions menu: ");
             System.out.println("____________________________________________________________________________");
             System.out.print("1: Add item | 2: Remove item |");
             System.out.println(" 3: Print items | 4: Sort by price | 5: Exit\n");
-            System.out.print("Choose the number: ");
+            System.out.print("Choose the option number: ");
 
             try {
                 userInput = scanner.nextInt();
@@ -39,7 +39,6 @@ public class Main {
                 System.out.println("Wrong Entry, try again.");
                 scanner.nextLine();
             }
-
         } while (true);
     }
 
@@ -62,42 +61,51 @@ public class Main {
     }
 
     public static void removeItem(){
-        boolean flag = true;
-        do {
-            System.out.print("\nEnter name of the item you want to delete (");
-            for (int i = 0; i < items.size(); i++) {
-                System.out.print((i < items.size() - 1) ? items.get(i).getName() + ", " : items.get(i).getName());
-            }
-            System.out.print("): ");
-            String input = scanner.next();
-
-            for (int i = 0; i < items.size(); i++) {
-                String name = items.get(i).getName();
-                String name2 = name.toLowerCase();
-                if (input.equals(name) || input.equals(name2)) {
-                    System.out.println(items.get(i).getName() + " deleted.");
-                    items.remove(items.get(i));
-                    flag = false;
+        if (items.isEmpty()) System.out.println("No added items yet.");
+        else {
+            boolean flag = true;
+            do {
+                System.out.print("\nEnter name of the item you want to delete (");
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.print((i < items.size() - 1) ? items.get(i).getName() + ", " : items.get(i).getName());
                 }
-            }
-            if (flag) System.out.println("Wrong entry, try again.");
-        } while (flag);
+                System.out.print("): ");
+                String input = scanner.next();
+
+                for (int i = 0; i < items.size(); i++) {
+                    String name = items.get(i).getName();
+                    String name2 = name.toLowerCase();
+                    if (input.equals(name) || input.equals(name2)) {
+                        System.out.println(items.get(i).getName() + " deleted.");
+                        items.remove(items.get(i));
+                        flag = false;
+                    }
+                }
+                if (flag) System.out.println("Wrong entry, try again.");
+            } while (flag);
+        }
     }
 
     public static void printItems(){
-        float sum = 0;
-        System.out.println();
-        for (Item item : items) {
-            String priceFormatted = NumberFormat.getCurrencyInstance().format(item.getPrice());
-            System.out.println(item.getName() + ": " + priceFormatted);
-            sum = sum + (item.getPrice());
+        if (items.isEmpty()) System.out.println("No added items yet.");
+        else {
+            float sum = 0;
+            System.out.println();
+            for (Item item : items) {
+                String priceFormatted = NumberFormat.getCurrencyInstance().format(item.getPrice());
+                System.out.println(item.getName() + ": " + priceFormatted);
+                sum = sum + (item.getPrice());
+            }
+            System.out.println("\n***** Total: " + NumberFormat.getCurrencyInstance().format(sum) + " *****");
+            // make it like list, frame list, make total,
         }
-        System.out.println("\n***** Total: " + NumberFormat.getCurrencyInstance().format(sum) + " *****");
-        // make it like list, frame list, make total,
     }
 
     public static void sortItems(){
+        if (items.isEmpty()) System.out.println("No added items yet.");
+        else {
 
+        }
     }
 }
     /*System.out.println("_________________________________________________________");
